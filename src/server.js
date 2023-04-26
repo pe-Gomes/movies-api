79b8b19a960bbfb5d8f6dd0ab47e4a00dc3071d7
pedro <pcgbernardes@gmail.com>
2023-04-26 12:23:24 -0400

    create dotenv and pm2

--- src/server.js
+++ src/server.js
@@ -32,6 +32,6 @@ app.use(( error, request, response, next) => {
   })
 });
 
-const PORT = 3333;
+const PORT = process.env.PORT;
 app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`));
 
